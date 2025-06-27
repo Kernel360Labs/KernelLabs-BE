2ea3ded8d4a9a0c65fdda006832243ad2645c80d
HoyiTT <pjs9177@naver.com>
2025-06-27 12:22:45 +0900

    [init]

---
+++ src/main/java/com/kernellabs/kernellabs/KernellabsApplication.java
@@ -0,0 +1,13 @@
+package com.kernellabs.kernellabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernellabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KernellabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kernellabs/kernellabs/KernellabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernellabs.kernellabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KernellabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
