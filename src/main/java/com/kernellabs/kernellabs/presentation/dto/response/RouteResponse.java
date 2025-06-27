5d495d0e37b08e3919092e9104173b969c4b1650
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-06-27 20:49:43 +0900

    [feat] #8 - AI 루트 추천 서비스 기능 추가

--- src/main/java/com/kernellabs/kernellabs/presentation/dto/response/RouteResponse.java
+++ src/main/java/com/kernellabs/kernellabs/presentation/dto/response/RouteResponse.java
@@ -1,4 +1,10 @@
 package com.kernellabs.kernellabs.presentation.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class RouteResponse {
+	private String route;
 }
